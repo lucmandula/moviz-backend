e253ca40d5d697f4572eb6fdc61432714b810e5a
Luc <mandulaluc@gmail.com>
2024-03-20 15:33:25 +0100

    moviz-backend

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+
+const apiKey = process.env.TMDB_API_KEY
+
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+
+router.get('/movies', (req, res) => {
+
+    fetch(url)
+    .then(res => res.json())
+    .catch(err => console.error('error'+ err))
+    .then(data => {
+        console.log(data.results)
+
+        let moviesData = [];
+        for (let movie of data.results) {
+            moviesData.push({ title:movie.title, overview:movie.overview.substring(0, 250)+"...", poster:`https://image.tmdb.org/t/p/w500/${movie.poster_path}`, voteAverage:movie.vote_average, voteCount:movie.vote_count })
+        }
+        res.json({  
+            result:true,
+            movies:moviesData
+        })
+    })
+})
+
+module.exports = router;
